55fee4621132c6bb7db118a03fbc0047ced825b4
Ernest Micklei <ernest.micklei@gmail.com>
2013-05-13 15:37:49 +0200

    add coverage script

---
+++ coverage.sh
@@ -0,0 +1,5 @@
+# https://github.com/matm/gocov-html
+# go get github.com/axw/gocov/gocov
+# go get github.com/matm/gocov-html
+
+gocov test github.com/emicklei/go-restful | gocov-html > restful.html | open restful.html
