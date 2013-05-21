29fb43253a080f946b31f81380a8c00a6c6da9ff
Ernest Micklei <ernest.micklei@gmail.com>
2013-05-21 22:44:05 +0200

    docs about filters and errors

--- doc.go
+++ doc.go
@@ -46,10 +46,98 @@ Example main:
 		log.Fatal(http.ListenAndServe(":8080", nil))
 	}
 
-Filter
+Filters
 
 A filter dynamically intercepts requests and responses to transform or use the information contained in the requests or responses.
+You can use filters to perform generic logging, measurement, authentication, redirect, set response headers etc.
+In the restful package there are three hooks into the request,response flow where filters can be added.
+Each filter must define a FilterFunction:
 
+	func (req *restful.Request, resp *restful.Response, chain *restful.FilterChain)
+
+
+Global Filters
+
+These are processed before any registered WebService.
+
+	// install a global filter (processed before any webservice)
+	restful.Filter(globalLogging)
+
+
+WebService Filters
+
+These are processed before any Route of a WebService.
+
+	// install a webservice filter (processed before any route)
+	ws.Filter(webserviceLogging).Filter(measureTime)
+
+
+Route Filters
+
+These are processed before calling the function associated with the Route.
+
+	// install 2 chained route filters (processed before calling findUser)
+	ws.Route(ws.GET("/{user-id}").Filter(routeLogging).Filter(NewCountFilter().routeCounter).To(findUser))
+
+
+Serving files
+
+Use the Go standard http.ServeFile function to serve file system assets.
+
+	ws.Route(ws.GET("/static/{resource}").To(staticFromPathParam))
+	...
+	// http://localhost:8080/static/test.xml
+	// http://localhost:8080/static/
+	func staticFromPathParam(req *restful.Request, resp *restful.Response) {
+		http.ServeFile(
+			resp.ResponseWriter,
+			req.Request,
+			path.Join(rootdir, req.PathParameter("resource")))
+	}
+
+Error Handling
+
+Unexpected things happen. If a request cannot be processed because of a failure, your service needs to tell the response what happened and why.
+For this reason HTTP status codes exist and it is important to use the correct code in every exceptional situation.
+
+400: Bad Request
+
+If path or query parameters are not valid (content or type) then use http.StatusBadRequest.
+
+	id, err := strconv.Atoi(req.PathParameter("id"))
+	if err != nil {
+		resp.WriteHeader(http.StatusBadRequest)
+		return
+	}
+
+404: Not Found
+
+Despite a valid URI, the resource requested may not be available
+
+	resp.WriteHeader(http.StatusNotFound)
+
+500: Internal Server Error
+
+If the application logic could not process the request (or write the response) then use http.StatusInternalServerError.
+
+	question, err := application.SharedLogic.GetQuestionById(id)
+	if err != nil {
+		log.Printf("GetQuestionById failed:", err)
+		resp.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
+ServiceError
+
+In addition to setting the correct (error) Http status code, you can choose to write a ServiceError message on the response:
+
+	resp.WriteEntity(restful.NewError(http.StatusBadRequest, "Non-integer {id} path parameter"))
+
+	resp.WriteEntity(restful.NewError(http.StatusInternalServerError, err.Error()))
+
+
+
+Resources
 
 [project]: https://github.com/emicklei/go-restful
 
@@ -59,6 +147,6 @@ A filter dynamically intercepts requests and responses to transform or use the i
 
 [1st use]: https://github.com/emicklei/landskape
 
-(c) 2012, http://ernestmicklei.com. MIT License
+(c) 2012,2013, http://ernestmicklei.com. MIT License
 */
 package restful
