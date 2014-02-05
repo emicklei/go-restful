5d79ecbae01f4076849e0a7eeac0a687b24606ab
Ernest Micklei <ernest.micklei@gmail.com>
2014-02-05 16:53:04 +0100

    reorganizing code before redesign

--- swagger/model_builder.go
+++ swagger/model_builder.go
@@ -23,75 +23,85 @@ func (b modelBuilder) addModel(st reflect.Type, nameOverride string) {
 		return
 	}
 	sm := Model{modelName, []string{}, map[string]ModelProperty{}}
-	// store before further initializing
+
+	// reference the model before further initializing (enables recursive structs)
 	b.Models[modelName] = sm
+
 	// check for structure or primitive type
-	if st.Kind() == reflect.Struct {
-		for i := 0; i < st.NumField(); i++ {
-			sf := st.Field(i)
-			jsonName := sf.Name
-			sft := sf.Type
-			prop := ModelProperty{}
-			required := true
-			// see if a tag overrides this
-			if jsonTag := st.Field(i).Tag.Get("json"); jsonTag != "" {
-				s := strings.Split(jsonTag, ",")
-				if s[0] == "-" {
-					continue
-				} else if s[0] != "" {
-					jsonName = s[0]
-				}
-				if len(s) > 1 {
-					switch s[1] {
-					case "string":
-						prop.Description = "(" + sft.String() + " as string)"
-						sft = reflect.TypeOf("")
-					case "omitempty":
-						required = false
-					}
-				}
-			}
-			if required {
-				sm.Required = append(sm.Required, jsonName)
-			}
-
-			prop.Type = sft.String() // include pkg path
-
-			// override type of list-likes
-			if sft.Kind() == reflect.Slice || sft.Kind() == reflect.Array {
-				prop.Type = "array"
-				elemName := b.getElementTypeName(modelName, jsonName, sft.Elem())
-				prop.Items = map[string]string{"$ref": elemName}
-				// add|overwrite model for element type
-				b.addModel(sft.Elem(), elemName)
-			} else if sft.Kind() == reflect.Ptr { // override type of pointer to list-likes
-				if sft.Elem().Kind() == reflect.Slice || sft.Elem().Kind() == reflect.Array {
-					prop.Type = "array"
-					elemName := b.getElementTypeName(modelName, jsonName, sft.Elem().Elem())
-					prop.Items = map[string]string{"$ref": elemName}
-					// add|overwrite model for element type
-					b.addModel(sft.Elem().Elem(), elemName)
-				} else {
-					// non-array, pointer type
-					prop.Type = sft.String()[1:] // no star, include pkg path
-					elemName := ""
-					if sft.Elem().Name() == "" {
-						elemName = modelName + "." + jsonName
-						prop.Type = elemName
-					}
-					b.addModel(sft.Elem(), elemName)
-				}
-			} else if sft.Name() == "" { // override type of anonymous structs
-				prop.Type = modelName + "." + jsonName
-				b.addModel(sft, prop.Type)
-			}
-
+	if st.Kind() != reflect.Struct {
+		return
+	}
+	for i := 0; i < st.NumField(); i++ {
+		sf := st.Field(i)
+		jsonName, prop := b.buildProperty(sf, &sm, modelName)
+		// add if not ommitted
+		if len(jsonName) != 0 {
 			sm.Properties[jsonName] = prop
 		}
 	}
 
 	// update model builder with completed model
 	b.Models[modelName] = sm
+}
+
+func (b modelBuilder) buildProperty(sf reflect.StructField, sm *Model, modelName string) (string, ModelProperty) {
+	jsonName := sf.Name
+	sft := sf.Type
+	prop := ModelProperty{}
+	required := true
+	// see if a tag overrides this
+	if jsonTag := sf.Tag.Get("json"); jsonTag != "" {
+		s := strings.Split(jsonTag, ",")
+		if s[0] == "-" {
+			return "", prop
+		} else if s[0] != "" {
+			jsonName = s[0]
+		}
+		if len(s) > 1 {
+			switch s[1] {
+			case "string":
+				prop.Description = "(" + sft.String() + " as string)"
+				sft = reflect.TypeOf("")
+			case "omitempty":
+				required = false
+			}
+		}
+	}
+	if required {
+		sm.Required = append(sm.Required, jsonName)
+	}
+
+	prop.Type = sft.String() // include pkg path
+
+	// override type of list-likes
+	if sft.Kind() == reflect.Slice || sft.Kind() == reflect.Array {
+		prop.Type = "array"
+		elemName := b.getElementTypeName(modelName, jsonName, sft.Elem())
+		prop.Items = map[string]string{"$ref": elemName}
+		// add|overwrite model for element type
+		b.addModel(sft.Elem(), elemName)
+	} else if sft.Kind() == reflect.Ptr { // override type of pointer to list-likes
+		if sft.Elem().Kind() == reflect.Slice || sft.Elem().Kind() == reflect.Array {
+			prop.Type = "array"
+			elemName := b.getElementTypeName(modelName, jsonName, sft.Elem().Elem())
+			prop.Items = map[string]string{"$ref": elemName}
+			// add|overwrite model for element type
+			b.addModel(sft.Elem().Elem(), elemName)
+		} else {
+			// non-array, pointer type
+			prop.Type = sft.String()[1:] // no star, include pkg path
+			elemName := ""
+			if sft.Elem().Name() == "" {
+				elemName = modelName + "." + jsonName
+				prop.Type = elemName
+			}
+			b.addModel(sft.Elem(), elemName)
+		}
+	} else if sft.Name() == "" { // override type of anonymous structs
+		prop.Type = modelName + "." + jsonName
+		b.addModel(sft, prop.Type)
+	}
+	return jsonName, prop
 }
 
 func (b modelBuilder) getElementTypeName(modelName, jsonName string, t reflect.Type) string {
