a40a6af9e165807a58ce90406d90fc1da165fda0
Guy Morgan <GuyMorgan@users.noreply.github.com>
2015-03-23 05:53:16 +0200

    Reduce StdLogger interface to minimum, conform callers

--- log/log.go
+++ log/log.go
@@ -5,22 +5,10 @@ import (
 	"os"
 )
 
-// Logger corresponds to a subset of the interface satisfied by stdlib log.Logger
+// Logger corresponds to a minimal subset of the interface satisfied by stdlib log.Logger
 type StdLogger interface {
-	Fatal(v ...interface{})
-	Fatalf(format string, v ...interface{})
-	Fatalln(v ...interface{})
-	// Flags() int
-	// Output(calldepth int, s string) error
-	// Panic(v ...interface{})
-	// Panicf(format string, v ...interface{})
-	// Panicln(v ...interface{})
-	// Prefix() string
 	Print(v ...interface{})
 	Printf(format string, v ...interface{})
-	Println(v ...interface{})
-	// SetFlags(flag int)
-	// SetPrefix(prefix string)
 }
 
 var Logger StdLogger
@@ -34,26 +22,10 @@ func SetLogger(customLogger StdLogger) {
 	Logger = customLogger
 }
 
-func Fatal(v ...interface{}) {
-	Logger.Fatal(v...)
-}
-
-func Fatalf(format string, v ...interface{}) {
-	Logger.Fatalf(format, v...)
-}
-
-func Fatalln(v ...interface{}) {
-	Logger.Fatalln(v...)
-}
-
 func Print(v ...interface{}) {
 	Logger.Print(v...)
 }
 
 func Printf(format string, v ...interface{}) {
 	Logger.Printf(format, v...)
-}
-
-func Println(v ...interface{}) {
-	Logger.Println(v...)
 }
