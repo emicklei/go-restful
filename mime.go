c5db81a814d410f759a39c4ad386fc403a09bf51
Ernest Micklei <emicklei@Ernests-iMac.local>
2016-01-30 13:52:44 +0100

    parse quality to float of mime spec

--- mime.go
+++ mime.go
@@ -1,12 +1,16 @@
 package restful
 
-import "strings"
+import (
+	"strconv"
+	"strings"
+)
 
 type mime struct {
 	media   string
-	quality string
+	quality float64
 }
 
+// insertMime adds a mime to a list and keeps it sorted by quality.
 func insertMime(l []mime, e mime) []mime {
 	for i, each := range l {
 		// if current mime has lower quality then insert before
@@ -18,13 +22,23 @@ func insertMime(l []mime, e mime) []mime {
 	return append(l, e)
 }
 
+// sortedMimes returns a list of mime sorted (desc) by its specified quality.
 func sortedMimes(accept string) (sorted []mime) {
 	for _, each := range strings.Split(accept, ",") {
-		q := strings.Split(strings.Trim(each, " "), ";")
-		if len(q) == 1 {
-			sorted = insertMime(sorted, mime{q[0], "q=1.0"})
+		typeAndQuality := strings.Split(strings.Trim(each, " "), ";")
+		if len(typeAndQuality) == 1 {
+			sorted = insertMime(sorted, mime{typeAndQuality[0], 1.0})
 		} else {
-			sorted = insertMime(sorted, mime{q[0], q[1]})
+			// take factor
+			parts := strings.Split(typeAndQuality[1], "=")
+			if len(parts) == 2 {
+				f, err := strconv.ParseFloat(parts[1], 64)
+				if err != nil {
+					traceLogger.Printf("unable to parse quality in %s, %v", each, err)
+				} else {
+					sorted = insertMime(sorted, mime{typeAndQuality[0], f})
+				}
+			}
 		}
 	}
 	return
