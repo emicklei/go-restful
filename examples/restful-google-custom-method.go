730608be270dd5ba9b563d0228cdec203cfcea36
ShiningRush <277040271@qq.com>
2019-09-07 18:21:27 +0800

    fix: custome method example

--- examples/restful-google-custom-method.go
+++ examples/restful-google-custom-method.go
@@ -10,18 +10,32 @@ import (
 // This example shows how to create a Route with google custom method
 // Requires the use of a CurlyRouter and path should end with the custom method
 //
-// GET http://localhost:8080/resource/some-resource-id:init
+// GET http://localhost:8080/resource:validate
+// POST http://localhost:8080/resource/some-resource-id:init
+// POST http://localhost:8080/resource/some-resource-id:recycle
 
 func main() {
 	DefaultContainer.Router(CurlyRouter{})
 	ws := new(WebService)
-	ws.Route(ws.GET("/basepath/{resourceId}:init").To(fromPathParam))
+
+	ws.Route(ws.GET("/resource:validate").To(validateHandler))
+	ws.Route(ws.POST("/resource/{resourceId}:init").To(initHandler))
+	ws.Route(ws.POST("/resource/{resourceId}:recycle").To(recycleHandler))
+
 	Add(ws)
 
 	println("[go-restful] serve path tails from http://localhost:8080/basepath")
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
 
-func fromPathParam(req *Request, resp *Response) {
-	io.WriteString(resp, "resourceId: "+req.PathParameter("resourceId"))
+func validateHandler(req *Request, resp *Response) {
+	io.WriteString(resp, "validate resource completed")
+}
+
+func initHandler(req *Request, resp *Response) {
+	io.WriteString(resp, "init resource completed, resourceId: "+req.PathParameter("resourceId"))
+}
+
+func recycleHandler(req *Request, resp *Response) {
+	io.WriteString(resp, "recycle resource completed, resourceId: "+req.PathParameter("resourceId"))
 }
