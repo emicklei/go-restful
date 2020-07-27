b42b5d7b0c329ac122a2eb0ea9c588deafe46622
Robert-André Mauchin <30413512+eclipseo@users.noreply.github.com>
2020-07-27 17:02:05 +0200

    Convert to string using rune() (#443)
    See https://github.com/golang/go/issues/32479
    
    Fix #442.
    
    Signed-off-by: Robert-André Mauchin <zebob.m@gmail.com>

--- bench_test.go
+++ bench_test.go
@@ -13,10 +13,10 @@ func setup(container *Container) {
 	rtCount := 26
 
 	for i := 0; i < wsCount; i++ {
-		root := fmt.Sprintf("/%s/{%s}/", string(i+97), string(i+97))
+		root := fmt.Sprintf("/%s/{%s}/", string(rune(i+97)), string(rune(i+97)))
 		ws := new(WebService).Path(root)
 		for j := 0; j < rtCount; j++ {
-			sub := fmt.Sprintf("/%s2/{%s2}", string(j+97), string(j+97))
+			sub := fmt.Sprintf("/%s2/{%s2}", string(rune(j+97)), string(rune(j+97)))
 			ws.Route(ws.GET(sub).To(echo))
 		}
 		container.Add(ws)
