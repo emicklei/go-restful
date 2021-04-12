002de13fa5f03e3c827ce62ab15a60aa0941f3de
ernest <ernest@kramphub.com>
2021-04-12 11:42:30 +0200

    fix handling no match access selected path

--- request.go
+++ request.go
@@ -114,11 +114,20 @@ func (r Request) Attribute(name string) interface{} {
 }
 
 // SelectedRoutePath root path + route path that matched the request, e.g. /meetings/{id}/attendees
+// If no route was matched then return an empty string.
 func (r Request) SelectedRoutePath() string {
+	if r.selectedRoute == nil {
+		return ""
+	}
+	// skip creating an accessor
 	return r.selectedRoute.Path
 }
 
 // SelectedRoute returns a reader to access the selected Route by the container
+// Returns nil if no route was matched.
 func (r Request) SelectedRoute() RouteReader {
+	if r.selectedRoute == nil {
+		return nil
+	}
 	return routeAccessor{route: r.selectedRoute}
 }
