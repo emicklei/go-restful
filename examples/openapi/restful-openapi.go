bc68247292e464b509764a8f469cae0fe5030b60
Ernest Micklei <ernest.micklei@gmail.com>
2022-11-14 22:41:48 +0100

    use path package to join slash fragments #519

--- examples/openapi/restful-openapi.go
+++ examples/openapi/restful-openapi.go
@@ -64,7 +64,6 @@ func (u UserResource) WebService() *restful.WebService {
 }
 
 // GET http://localhost:8080/users
-//
 func (u UserResource) findAllUsers(request *restful.Request, response *restful.Response) {
 	list := []User{}
 	for _, each := range u.users {
@@ -74,7 +73,6 @@ func (u UserResource) findAllUsers(request *restful.Request, response *restful.R
 }
 
 // GET http://localhost:8080/users/1
-//
 func (u UserResource) findUser(request *restful.Request, response *restful.Response) {
 	id := request.PathParameter("user-id")
 	usr := u.users[id]
@@ -87,7 +85,6 @@ func (u UserResource) findUser(request *restful.Request, response *restful.Respo
 
 // PUT http://localhost:8080/users/1
 // <User><Id>1</Id><Name>Melissa Raspberry</Name></User>
-//
 func (u *UserResource) updateUser(request *restful.Request, response *restful.Response) {
 	usr := new(User)
 	err := request.ReadEntity(&usr)
@@ -101,7 +98,6 @@ func (u *UserResource) updateUser(request *restful.Request, response *restful.Re
 
 // PUT http://localhost:8080/users/1
 // <User><Id>1</Id><Name>Melissa</Name></User>
-//
 func (u *UserResource) createUser(request *restful.Request, response *restful.Response) {
 	usr := User{ID: request.PathParameter("user-id")}
 	err := request.ReadEntity(&usr)
@@ -114,7 +110,6 @@ func (u *UserResource) createUser(request *restful.Request, response *restful.Re
 }
 
 // DELETE http://localhost:8080/users/1
-//
 func (u *UserResource) removeUser(request *restful.Request, response *restful.Response) {
 	id := request.PathParameter("user-id")
 	delete(u.users, id)
@@ -176,7 +171,7 @@ func enrichSwaggerObject(swo *spec.Swagger) {
 
 // User is just a sample type
 type User struct {
-	ID   string `json:"id" description:"identifier of the user"`
-	Name string `json:"name" description:"name of the user" default:"john"`
-	Age  int    `json:"age" description:"age of the user" default:"21"`
+	ID   string `xml:"id" json:"id" description:"identifier of the user"`
+	Name string `xml:"name" json:"name" description:"name of the user" default:"john"`
+	Age  int    `xml:"age" json:"age" description:"age of the user" default:"21"`
 }
